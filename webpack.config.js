78a374db5f0052ccb9224a6bf4033c91e4929040
UCDM <palama.ucdm.1212@gmail.com>
2022-01-10 00:45:01 +0900

    ファイルネームを保ったまま画像をbuild

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             loader: "file-loader",
             options: {
               esModule: false,
-              name: 'images/icon.jpg'
+              name: 'images/[name].[ext]'
             }
           }
         ]
