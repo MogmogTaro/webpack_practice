e24f39608d1e6a01a35e409dd06d8fbb88b4ef44
UCDM <palama.ucdm.1212@gmail.com>
2022-01-11 13:47:24 +0900

    add tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
         test: /\.js/,
         exclude: /node_modules/,
         use: [
